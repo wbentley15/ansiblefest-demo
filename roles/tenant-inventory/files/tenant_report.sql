07927c7b824b422570d85516bf10f1502bd89c78
wbentley15 <wbentley@hitchnyc.com>
2015-10-02 10:32:39 -0400

    .

--- roles/tenant-inventory/files/tenant_report.sql
+++ roles/tenant-inventory/files/tenant_report.sql
@@ -1,4 +1,4 @@
-USE keystone;
+USE nova;
 SELECT SUM(instances.vcpus) as vCPU, SUM(instances.memory_mb) as memory, SUM(instances.root_gb) as disk, keystone.project.name as tenant from instances
 INNER JOIN keystone.project ON
 instances.project_id=keystone.project.id 
