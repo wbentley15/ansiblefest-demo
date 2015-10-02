80b5790b853184aa142f424a5df14e1b4f2d3cf5
wbentley15 <wbentley@hitchnyc.com>
2015-10-02 13:06:23 -0400

    final

--- roles/cloud-inventory/files/cloud_report.sql
+++ roles/cloud-inventory/files/cloud_report.sql
@@ -2,9 +2,10 @@ USE keystone;
 SELECT count(*) as total_users from user WHERE user.enabled=1;
 SELECT count(*) as total_tenants from project WHERE project.enabled=1;
 USE cinder;
-SELECT count(*) as total_volumes from volumes WHERE volumes.status='available';
+SELECT count(*) as total_volumes, SUM(volumes.size) as total_volume_usage_GB from volumes
+WHERE volumes.status='available';
 USE neutron;
 SELECT count(*) as total_networks from networks WHERE networks.status='ACTIVE';
 USE nova;
-SELECT SUM(instances.vcpus) as total_vCPU, SUM(instances.memory_mb) as total_memory, SUM(instances.root_gb) as total_disk from instances
+SELECT SUM(instances.vcpus) as total_vCPU, SUM(instances.memory_mb) as total_memory_MB, SUM(instances.root_gb) as total_disk_GB from instances
 WHERE instances.vm_state='active';
