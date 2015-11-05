d7fcba5e581b11f27f3965d2f2aa9d173a65eb2e
wbentley15 <wbentley@hitchnyc.com>
2015-11-05 10:28:02 -0500

    .

--- roles/network-inventory/files/network_report.sql
+++ roles/network-inventory/files/network_report.sql
@@ -1,6 +1,5 @@
 USE neutron;
 SELECT networks.id, networks.name, subnets.name as subnet, subnets.cidr, networks.status, networks.shared, keystone.project.name as tenant from networks
-INNER JOIN keystone.project ON
-networks.tenant_id COLLATE utf8_unicode_ci = keystone.project.id INNER JOIN
-subnets ON networks.id=subnets.network_id
+INNER JOIN keystone.project ON networks.tenant_id COLLATE utf8_unicode_ci = keystone.project.id 
+INNER JOIN subnets ON networks.id=subnets.network_id
 ORDER BY tenant;
