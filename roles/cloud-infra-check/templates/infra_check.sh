8946379cc99ad4609ac0d0ae9cf132f04d1f3e59
wbentley15 <wbentley@hitchnyc.com>
2015-11-28 13:21:21 -0600

    .

--- roles/cloud-infra-check/templates/infra_check.sh
+++ roles/cloud-infra-check/templates/infra_check.sh
@@ -5,15 +5,12 @@
 NOW=$(date +"%Y%m%d")
 
 # Check health of MariaDB and Galera
-ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py galera_container -m shell \ 
--a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
+ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
 
 # Check health of RabbitMQ
-ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py rabbit_mq_container -m shell \ 
--a "rabbitmqctl cluster_status" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
+ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py rabbit_mq_container -m shell -a "rabbitmqctl cluster_status" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
 
 # Check queue health of RabbitMQ
-ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py rabbit_mq_container -m shell \ 
--a "rabbitmqctl list_queues | awk '\$2>0'" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
+ansible -i {{ DEPLOY_LOC }}/inventory/dynamic_inventory.py rabbit_mq_container -m shell -a "rabbitmqctl list_queues | awk '\$2>0'" >> {{ REPORT_DIR }}/os_health_check_$NOW.txt
 
 exit 0
