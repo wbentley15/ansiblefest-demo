4e56244377de36a66590ce50d43942e4b2d856e6
wbentley15 <wbentley@hitchnyc.com>
2015-11-28 12:33:35 -0600

    .

--- roles/cloud-infra-check/files/infra_check.sh
+++ roles/cloud-infra-check/files/infra_check.sh
@@ -5,14 +5,14 @@
 
 # Check health of MariaDB and Galera
 ansible galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'" \
->> $1/os_health_check_('pipe', 'date +%Y%m%d').txt
+>> $1/os_health_check_(date +"%Y%m%d").txt
 
 # Check health of RabbitMQ
 ansible rabbit_mq_container -m shell -a "rabbitmqctl cluster_status" \
->> $1/os_health_check_('pipe', 'date +%Y%m%d').txt
+>> $1/os_health_check_(date +"%Y%m%d").txt
 
 # Check queue health of RabbitMQ
 ansible rabbit_mq_container -m shell -a "rabbitmqctl list_queues | awk '\$2>0'" \
->> $1/os_health_check_('pipe', 'date +%Y%m%d').txt
+>> $1/os_health_check_(date +"%Y%m%d").txt
 
 exit 0
