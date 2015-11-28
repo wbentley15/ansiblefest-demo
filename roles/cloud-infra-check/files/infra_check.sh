e046cb356f66eff2f6e6a5e7fe68d488a7e372dc
wbentley15 <wbentley@hitchnyc.com>
2015-11-28 12:36:21 -0600

    .

--- roles/cloud-infra-check/files/infra_check.sh
+++ roles/cloud-infra-check/files/infra_check.sh
@@ -2,17 +2,18 @@
 # Script to automate OpenStack cloud Infrastructure services health check
 #
 #
+NOW=$(date +"%Y%m%d")
 
 # Check health of MariaDB and Galera
 ansible galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'" \
->> $1/os_health_check_(date +"%Y%m%d").txt
+>> $1/os_health_check_$NOW.txt
 
 # Check health of RabbitMQ
 ansible rabbit_mq_container -m shell -a "rabbitmqctl cluster_status" \
->> $1/os_health_check_(date +"%Y%m%d").txt
+>> $1/os_health_check_$NOW.txt
 
 # Check queue health of RabbitMQ
 ansible rabbit_mq_container -m shell -a "rabbitmqctl list_queues | awk '\$2>0'" \
->> $1/os_health_check_(date +"%Y%m%d").txt
+>> $1/os_health_check_$NOW.txt
 
 exit 0
